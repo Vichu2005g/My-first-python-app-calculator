74991759bce57c4e30a20ecfd19bdcf577fb9507
Vichu2005g <69617771+Vichu2005g@users.noreply.github.com>
2021-01-19 13:07:00 +0530

    exponent added

--- clac.py
+++ clac.py
@@ -1,4 +1,4 @@
-x = input('hi! Enter 1 for addition, 2 for subtraction, 3 for multiplication and 4 for division: ')
+x = input('hi! Enter 1 for addition, 2 for subtraction, 3 for multiplication and 4 for division and 5 for exponet: ')
 
 if x == '1' :
   a = float(input("first number: "))
@@ -15,6 +15,10 @@ elif x == '3':
 elif x == '4':
   aaaa = float(input("first number: "))
   bbbb  = float(input("second number: "))  
-  print(aaaa / bbbb ,'your reminder is', aaaa % bbbb )  
+  print(aaaa / bbbb ,'your reminder is', aaaa % bbbb ) 
+elif x == '5':
+  aaaaa = float(input("first number (this would be your whole number): "))
+  bbbbb = float(input("second number (this would be the power for the number): "))
+  print(aaaaa ** bbbbb)   
 else:
   print("please write a valid option! Exitting...")
