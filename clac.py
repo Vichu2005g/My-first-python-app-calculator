00a3da55ca141088ae0cffda7305e2f9ea9d249f
Vichu2005g <69617771+Vichu2005g@users.noreply.github.com>
2021-01-19 14:14:30 +0530

    New added stuff: exponent, nth term of an ap and sum of nth term of an ap!

--- clac.py
+++ clac.py
@@ -1,5 +1,4 @@
-x = input('hi! Enter 1 for addition, 2 for subtraction, 3 for multiplication and 4 for division and 5 for exponet: ')
-
+x = input('hi! Enter 1 for addition, 2 for subtraction, 3 for multiplication, 4 for division, 5 for exponet, 6 for nth term of ap and 7 for sum of nth term of an ap: ')
 if x == '1' :
   a = float(input("first number: "))
   b = float(input("second number: "))
@@ -19,6 +18,18 @@ elif x == '4':
 elif x == '5':
   aaaaa = float(input("first number (this would be your whole number): "))
   bbbbb = float(input("second number (this would be the power for the number): "))
-  print(aaaaa ** bbbbb)   
+  print(aaaaa ** bbbbb) 
+elif x =='6':
+  Ath = float(input("first term a: "))
+  d = float(input("common difference d: "))
+  n = float(input("the nth term of ap n: "))
+  print(Ath + (n - 1)*d)    
+elif x == '7':
+  Ath1 = float(input("first term a: "))
+  d1 = float(input("common difference d: "))
+  n1 = float(input("nth term of ap n: "))
+  print(n1/2*(2*Ath1 + (n1-1)*d1))  
 else:
   print("please write a valid option! Exitting...")
+
+
